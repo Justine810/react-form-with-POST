d3c0ff19a9457b8abb058ce8b2ba8d5423175bd3
Justine810 <justine.gautreau@gmail.com>
2020-05-04 11:34:02 +0200

    quete13

---
+++ src/FormMovie.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class FormMovie extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          title: '',
+          poster: '',
+          comment: '',
+        }
+        this.onChange = this.onChange.bind(this);
+        this.submitForm = this.submitForm.bind(this);
+      }
+
+
+onChange(e) 
+    {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  
+submitForm(e) 
+    {
+    const url = 'https://post-a-form.herokuapp.com/api/movies';
+    e.preventDefault();
+    axios.post(url, this.state)
+  .then(res => res.data)
+  .then(res => {
+    alert(`Your movie has been added with the ID ${res.id} !`);
+  })
+  .catch(e => {
+    console.error(e);
+    alert(`Error during recording your movie : ${e.message}`);
+  });
+  }
+
+  render() {
+    return (
+<div className="FormMovie">
+  <h1>Add your favourite movie</h1>
+
+  <form onSubmit={this.submitForm}>
+    <fieldset>
+      <legend>Informations</legend>
+      <div className="form-data">
+        <label htmlFor="title">Name</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          onChange={this.onChange}
+          value={this.state.title}
+        />
+      </div>
+
+      <div className="form-data">
+        <label htmlFor="poster">Poster</label>
+        <input
+          type="text"
+          id="poster"
+          name="poster"
+          onChange={this.onChange}
+          value={this.state.poster}
+        />
+      </div>
+
+      <div className="form-data">
+        <label htmlFor="comment">Comment</label>
+        <input
+          type="textarea"
+          id="comment"
+          name="comment"
+          onChange={this.onChange}
+          value={this.state.comment}
+        />
+      </div>
+      <hr />
+      <div className="form-data">
+        <input type="submit" value="Envoyer" />
+      </div>
+    </fieldset>
+  </form>
+</div>
+    );
+  }
+}
+
+export default FormMovie
